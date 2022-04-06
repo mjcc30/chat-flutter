dcf1857390d52050c79e1047f6d42af7fccac2e1
djinodissingar <dissingardjino@icloud.com>
2022-04-06 10:34:02 +0200

    page connexion

--- lib/main.dart
+++ lib/main.dart
@@ -25,6 +25,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
@@ -48,68 +49,160 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+//Variable
+late String mail;
+late String password;
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+
     return Scaffold(
+     
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
+        
+        centerTitle: true,
+       
+    
         title: Text(widget.title),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      body: Container(
+        padding: const EdgeInsets.all(20),
+        child: bodyPage(),
+      )
+       // This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+
+  Widget bodyPage(){
+    return 
+    
+    /*const Text('coucou',
+    style: TextStyle(
+      fontSize: 30,
+      color: Colors.blue
+      ),
+);
+     Container(
+       height: 300,
+       width: 250,
+       //color: Colors.red,
+       decoration: const BoxDecoration(
+         shape: BoxShape.circle,
+         color: Colors.blue
+       ),
+     );
+     
+    
+    const TextField(
+      obscureText: true,
+
+    );
+    
+    SingleChildScrollView(
+      scrollDirection: Axis.horizontal,
+      
+      child: Row(
+
+  
+      children: [
+        Container(
+          height: 200,
+          width: 200,
+          color: Colors.yellow,
+          ),
+           Container(
+          height: 200,
+          width: 200,
+          color: Colors.red,
+          ),
+           Container(
+          height: 200,
+          width: 200,
+          color: Colors.cyan,
+          ),
+           Container(
+          height: 200,
+          width: 200,
+          color: Colors.green,
+          ),
+           Container(
+          height: 200,
+          width: 200,
+          color: Colors.purple,
+          ),
+           Container(
+          height: 200,
+          width: 200,
+          color: Colors.yellow,
+          ),
+
+      ],
+
+    ),
+    );
+    
+    Card(
+      elevation: 5.0,
+      child: Text("Mon text est élevé"),
+    );
+    
+    ElevatedButton(
+      
+      onPressed: (){
+        print("J'ai appuyé sur le bouton");
+      }, 
+      child: const Text("Connexion")
+      );
+  */
+  Column(
+    children: [
+      TextField(
+        decoration: InputDecoration(
+          prefixIcon: const Icon(Icons.mail),
+         hintText: "Taper votre adresse mail",
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(20),
+        ),
+        ),
+
+        onChanged: (value){
+          setState(() {
+            mail = value;
+          });
+          
+        },
+      ),
+      const SizedBox(height: 10,),
+       TextField(
+          decoration: InputDecoration(
+          prefixIcon: const Icon(Icons.lock_rounded),
+         hintText: "Taper votre mot de passe",
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(20),
+        ),
+        ),
+         obscureText: true,
+         onChanged: (value){
+           setState(() {
+             password = value;
+           });
+         },
+       ),
+       const SizedBox(height: 10,),
+       ElevatedButton(
+         style: ElevatedButton.styleFrom(
+           primary: Colors.amber,
+           shape: RoundedRectangleBorder(borderRadius:BorderRadius.circular(10))
+         ),
+         onPressed: (){
+
+         }, 
+         child: const Text("Connexion")
+         )
+    ],
+
+  );
+
+
   }
 }
